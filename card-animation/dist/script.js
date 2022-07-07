d26da4532b821827b218237fe1c1cdc5099000f5
Logan Zechella <jloganzechella@gmail.com>
2022-07-07 16:31:30 -0400

    SCSS styling added. WORK IN PROGRESS

--- card-animation/dist/script.js
+++ card-animation/dist/script.js
@@ -35,3 +35,13 @@ function toggleActive () {
 
 // Invoke "start ()" function
 window.addEventListener ('load', start);
+
+
+(function () {
+  var burger = document.querySelector('.burger-container'),
+    header = document.querySelector('.header');
+
+  burger.onclick = function () {
+    header.classList.toggle('menu-opened');
+  }
+}());
